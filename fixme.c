3a1994c5871c915828caee1dcaa1242dd28b31a2
0x90eax <137312046+0x90eax@users.noreply.github.com>
2023-08-02 21:19:49 -0700

    Add files via upload

---
+++ fixme.c
@@ -0,0 +1,9 @@
+#include<.h>
+
+int main(){
+__int128 p = *(__int128*) "\x04\x06\x06\x06\x04\x04\x04\x07\x07\x07\x0a\x0b\x0c\x0d\x0e\x0f";
+        printf("What's this number in octal? %s\n", p);
+int var;
+        scanf("%d", var);
+        printf("Take the octal value and xor it with the first value. %d\n, var);
+}
